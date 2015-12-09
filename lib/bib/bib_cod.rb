100572310c23fc6ccaae058e950c64310d689267
alu0100843101 <alu0100843101@ull.edu.es>
2015-12-09 12:23:47 +0000

    Practica 10 V2

--- lib/bib/bib_cod.rb
+++ lib/bib/bib_cod.rb
@@ -1,11 +1,11 @@
 class Bib
  
- attr_accessor :autor, :titulo, :serie, :editorial, :edicion, :fecha, :isbn #RW
+ attr_accessor :autor, :apellidos, :titulo, :serie, :editorial, :edicion, :fecha, :isbn #RW
  include Comparable # Módulo que permite que implementemos comparaciones convencionales en nuestra clase.
  
- def initialize(autor, titulo, serie, editorial, edicion, fecha, isbn)
-   @autor, @titulo, @serie, @editorial, @edicion, @fecha, @isbn = autor, titulo, serie, editorial, edicion, fecha, isbn
- end
+    def initialize(autor, apellidos, titulo, serie, editorial, edicion, fecha, isbn)
+        @autor, @apellidos, @titulo, @serie, @editorial, @edicion, @fecha, @isbn = autor, apellidos, titulo, serie, editorial, edicion, fecha, isbn
+    end
 
     def <=>(other) # Método para incluir el mixin comparable. 
         if other.is_a? Bib
@@ -18,8 +18,6 @@ class Bib
     def ==(other) # Método de comparación añadido al mixin comparable.
         if other.is_a? Bib
           @titulo == other.titulo
-        else
-          false
         end
     end
     
@@ -29,7 +27,13 @@ class Bib
  end
     
  def get_autor
-  @autor
+   size = @autor.length
+    i = 0
+    while i < (size-1)
+        cadena = "#{cadena}"+"#{@autor[i]} #{@apellidos[i]}, "
+        i = i+1
+    end
+    cadena = "#{cadena}"+"#{@autor[i]} #{@apellidos[i]}"
  end
     
  def get_titulo
@@ -61,8 +65,8 @@ end
 class Libro < Bib
     attr_reader :isbnl
     
-    def initialize (autor, titulo, serie, editorial, edicion, fecha, isbnl)
-        super(autor, titulo, serie, editorial, edicion, fecha, isbnl)
+    def initialize (autor, apellidos, titulo, serie, editorial, edicion, fecha, isbnl)
+        super(autor, apellidos, titulo, serie, editorial, edicion, fecha, isbnl)
         @isbnl=isbnl
     end
     
@@ -71,8 +75,8 @@ end
 class Revista < Bib
     attr_reader :issn # Numero de serie 
     
-    def initialize (autor, titulo, serie, editorial, edicion, fecha, issn)
-        super(autor, titulo, serie, editorial, edicion, fecha, nil)
+    def initialize (autor, apellidos, titulo, serie, editorial, edicion, fecha, issn)
+        super(autor, apellidos, titulo, serie, editorial, edicion, fecha, nil)
         @issn=issn
     end
 end
@@ -80,8 +84,8 @@ end
 class Periodico < Bib
     attr_reader :columnas
     
-    def initialize (autor, titulo, serie, editorial, edicion, fecha, columnas)
-        super(autor, titulo, serie, editorial, edicion, fecha, nil)
+    def initialize (autor, apellidos, titulo, serie, editorial, edicion, fecha, columnas)
+        super(autor, apellidos, titulo, serie, editorial, edicion, fecha, nil)
         @columnas=columnas
     end
 end
@@ -89,13 +93,13 @@ end
 class Electronico < Bib
     attr_reader :url
     
-    def initialize (autor, titulo, serie, editorial, edicion, fecha, url)
-        super(autor, titulo, serie, editorial, edicion, fecha, nil)
+    def initialize (autor, apellidos, titulo, serie, editorial, edicion, fecha, url)
+        super(autor, apellidos, titulo, serie, editorial, edicion, fecha, nil)
         @url=url
     end
 end
 
-################################################################################
+#################################################################################################################################
 
 Node = Struct.new :value, :next, :prev
 
@@ -109,13 +113,13 @@ class LinkedList
         	@end = nil
 		end
 		
-	def each 
-        aux = @head 
-        while aux != nil 
-          yield aux.value 
-          aux = aux.next 
+	    def each 
+            aux = @head 
+            while aux != nil 
+                yield aux.value 
+                aux = aux.next 
+            end
         end
-    end
 		
 		def pushf(value) # Se inserta por el final.
 			 nodo = Node.new(value,nil,nil)
@@ -139,7 +143,7 @@ class LinkedList
             	@head.prev = nodo
             	@head = nodo
         	end
-  end
+        end
 		
 		def popf() # Se extrae por el final.
         	if(@head==nil)
@@ -154,19 +158,91 @@ class LinkedList
             	end
             	return valor
         	end
-  end
+        end
     	
-  def popi() # Se extrae por el inicio.
-    	if(@head==nil)
+        def popi() # Se extrae por el inicio.
+        	if(@head==nil)
         		return false
-    	else
-        	 valor = @head.value
-          @head = @head.next
-        	 if(vacia == false)
-            		@head.prev = nil
-          end
-            	return valor
-     end	
-  end
+    	    else
+            	valor = @head.value
+                @head = @head.next
+            	if(vacia == false)
+            	    @head.prev = nil
+                end
+                return valor
+            end	
+        end
+end
+
+class Cita < LinkedList
+    include Comparable
+    attr_reader :bib
     
+    def initialize (b)
+       @bib=b 
+    end
+
+    def get_autor
+        size = @bib.autor.length
+        i = 0
+        while i < (size-1)
+            cadena = "#{cadena}"+"Autor, "
+            cadena = "#{cadena}"+"#{@bib.apellidos[i][0]}. #{@bib.autor[i][0]}. & "
+            i = i+1
+        end
+        cadena = "#{cadena}"+"Autor, "
+        cadena = "#{cadena}"+"#{@bib.apellidos[i][0]}. #{@bib.autor[i][0]}. "
+    end
+    
+    def <=>(other)
+        return nil unless other.is_a? Cita
+        if(@bib.apellidos == other.bib.apellidos)
+            if(@bib.fecha == other.bib.fecha)
+                @bib.titulo <=> other.bib.titulo
+            else
+                @bib.fecha <=> other.bib.fecha
+            end
+        else
+            @bib.apellidos <=> other.bib.apellidos
+        end
+    end
+    
+    def get_titulo
+        size = @bib.titulo.length
+        @bib.titulo[0] = @bib.titulo[0].capitalize # Poner en mayúsculas.
+        i=0
+        while i < size
+            if (@bib.titulo[i] == " ")
+                bib.titulo[i+1] = bib.titulo[i+1].capitalize
+            end
+            i = i+1
+        end
+        @bib.titulo
+    end
+    
+    def to_s
+        if @bib.instance_of?Libro
+            cadena = "#{get_autor}(#{@bib.fecha}).  "
+            cadena = "#{cadena}"+"Titulo del libro: #{@bib.get_titulo} (#{@bib.get_edicion}).  "
+            cadena = "#{cadena}"+"Lugar de publicacion: #{@bib.get_editorial}.  "
+        end
+        if @bib.instance_of?Revista
+            cadena = "#{get_autor}(#{@bib.fecha}).  "
+            cadena = "#{cadena}"+"Titulo de la revista: #{@bib.get_titulo} (#{@bib.get_edicion}).  "
+            cadena = "#{cadena}"+"Lugar de publicacion: #{@bib.get_editorial}.  "
+        end
+        if @bib.instance_of?Periodico
+            cadena = "#{get_autor}(#{@bib.fecha}).  "
+            cadena = "#{cadena}"+"Titulo del articulo: #{@bib.get_titulo} (#{@bib.get_edicion}).  "
+            cadena = "#{cadena}"+"Columnas: #{@bib.columnas}.  "
+            cadena = "#{cadena}"+"Lugar de publicacion: #{@bib.get_editorial}.  "
+        end
+        if @bib.instance_of?Electronico
+            cadena = "#{get_autor}(#{@bib.fecha}).  "
+            cadena = "#{cadena}"+"Titulo: #{@bib.get_titulo} (#{@bib.get_edicion}).  "
+            cadena = "#{cadena}"+"Lugar de publicacion: #{@bib.get_editorial}.  "
+            cadena = "#{cadena}"+"Disponible en: #{@bib.url}  "
+        end
+        cadena
+    end
 end
--- spec/bib_spec.rb
+++ spec/bib_spec.rb
@@ -1,15 +1,26 @@
 require 'spec_helper'
 require 'bib/bib_cod'
 
-describe Bib do
+describe Cita do
     
     before :each do
-        @b1 = Bib.new("Ricky Seawton", "Ruby Instances", nil, "Santillanic", "1 edition", "17 July, 2009", "323")
-        @b2 = Bib.new("Aba Meawen", "Invented Title", nil, "Paraninfo", "1 edition", "17 July, 2009", "5874528")
+        @l = Libro.new(['Jesús', 'José'], ['Thrashorras', 'Luis'], 'Configuración de instalaciones eléctricas', nil, 'Paraninfo', '2013 edition', '2013', '9788497329354')
+        @r = Revista.new(['Felipe'], ['Clavo'], 'Decoracion de hogar', nil, 'Hogares', '1 edition', 2015, 9857425)
+        @p = Periodico.new(['Maria'], ['Darias'], 'Noticias', nil, 'Diario de avisos', '10 edition', 2013, 15)
+        @e = Electronico.new(['Alexis'], ['Ramon'], 'Programa en Java', nil, 'Programming', '17 edition', 2015, 'www.urldelbook.com')
+        
+        @c1 = Cita.new(@l)
+        @c2 = Cita.new(@r)
+        @c3 = Cita.new(@p)
+        @c4 = Cita.new(@e)
+
+        @citas = LinkedList.new()
     end
-    
-    it "Las referencias no son iguales" do
-      expect(@b1 == @b2).to eq(false)
+
+    it "Texto Formateado con las condiciones establecidas" do
+            expect(@c1.to_s).to eq('Autor, T. J. & Autor, L. J. (2013).  Titulo del libro: Configuración de instalaciones eléctricas (2013 edition).  Lugar de publicacion: Paraninfo.  ')
+            expect(@c2.to_s).to eq('Autor, C. F. (2015).  Titulo de la revista: Decoracion de hogar (1 edition).  Lugar de publicacion: Hogares.  ')
+            expect(@c3.to_s).to eq('Autor, D. M. (2013).  Titulo del articulo: Noticias (10 edition).  Columnas: 15.  Lugar de publicacion: Diario de avisos.  ')
+            expect(@c4.to_s).to eq('Autor, R. A. (2015).  Titulo: Programa en Java (17 edition).  Lugar de publicacion: Programming.  Disponible en: www.urldelbook.com  ')
     end
-    
 end
